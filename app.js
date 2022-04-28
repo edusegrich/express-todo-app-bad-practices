a388ebe49dad6dd7b17876859b3add1e74e474a2
Eduardo Segura Richart <edusr@correo.ugr.es>
2022-04-28 20:24:17 +0200

    Clean code changes

--- app.js
+++ app.js
@@ -1,52 +1,46 @@
-// === imports == //
-express = require('express')
-bodyParser = require('body-parser')
+import express from 'express';
+import bodyParser from 'body-parser';
 
-
-// === initialisation == //
-app = express()
+const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+const todoItems = [];
+todoItems.push({ id: 1, value: 'learn react', done: false });
+todoItems.push({ id: 2, value: 'Go shopping', done: true });
+todoItems.push({ id: 3, value: 'buy flowers', done: true });
+let lastId = todoItems.length;
 
-// === store == //
-var todoItems = [];
-todoItems.push({ index: 1, value: "learn react", done: false });
-todoItems.push({ index: 2, value: "Go shopping", done: true });
-todoItems.push({ index: 3, value: "buy flowers", done: true });
-var index = 5;
+app.get('/', (req, res) => res.send('Hello World!'));
 
-
-// === endpoints == //
-// index endpoint
-app.get('/', (req, res) => res.send('Hello World!'))
-
-// get all tasks
 app.get('/task', (req, res) => {
-    return res.json({ data: todoItems, status: "success" })
-})
+	return res.json({ data: todoItems, status: 'Success in getting all tasks' });
+});
 
-// create a task
 app.post('/task', (req, res) => {
-    todoItems.push({
-        index: index++,
-        value: req.body.value,
-        done: false,
-    })
-    return res.json({ data: todoItems, status: 'success' })
-})
+	lastId = todoItems.push({
+		id: lastId + 1,
+		value: req.body.value,
+		done: false,
+	});
+	return res.json({ data: todoItems, status: 'Success in creating a task' });
+});
 
-// delete a task
 app.delete('/task/:id', (req, res) => {
-    var todoItems = todoItems.filter(d => d.index != +req.params.id)
-    return res.json({ data: todoItems, status: 'success' })
-})
+	const todoItems = todoItems.filter(item => item.id !== +req.params.id);
+	return res.json({ data: todoItems, status: 'Success in deleting the task' });
+});
 
-// update a task
 app.patch('/task/:id', (req, res) => {
-    todoItems.filter(d => d.index == +req.params.id)[0].done = req.body.done
-    return res.json({ data: todoItems, status: 'success' })
-})
+	const task = todoItems.find(item => item.id === +req.params.id);
+	task.done = req.body.done;
+	return res.json({ data: todoItems, status: 'Success in updating the task' });
+});
 
-// === run app == //
-app.listen(8000, () => console.log(`Example app running!`))
+app.listen(8000, () => {
+	console.log('Example app running!');
+	console.log();
+	console.log('Running on:');
+	console.log();
+	console.log('	- Local:	"localhost:8000".');
+});
